29ac06ddd2bbcf180769b32eac6c475affbb75ac
isaactl <yefeng1223@qq.com>
2024-11-27 16:17:11 +0800

    remove close button

--- src/components/diff/editor.js
+++ src/components/diff/editor.js
@@ -1,22 +1,6 @@
-import React, {useRef} from 'react'
-import {MonacoDiffEditor} from 'react-monaco-editor'
-import {Dialog} from "@material-ui/core";
-import CloseIcon from '@material-ui/icons/Close';
-import {makeStyles} from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-
-const useStyles = makeStyles(theme => ({
-    appBar: {
-        position: 'relative',
-    },
-    title: {
-        marginLeft: theme.spacing(2),
-        flex: 1,
-    },
-}));
+import { Dialog } from "@material-ui/core";
+import React, { useRef } from 'react';
+import { MonacoDiffEditor } from 'react-monaco-editor';
 
 const options = {
     selectOnLineNumbers: true,
@@ -25,17 +9,10 @@ const options = {
     originalEditable: true,
     wordWrap: "on",
     // readOnly: !props.editable
-}
+};
 
 export default function DiffEditor(props) {
-    const classes = useStyles()
-    const monacoRef = useRef(null)
-
-    function handleChange(newValue, e) {
-    }
-
-    function onCancel() {
-    }
+    const monacoRef = useRef(null);
 
     // // https://github.com/react-monaco-editor/react-monaco-editor/blob/master/src/diff.js#L19
     // function onSave() {
@@ -45,16 +22,6 @@ export default function DiffEditor(props) {
 
     return (
         <Dialog fullScreen open={props.open} onClose={onCancel}>
-            <AppBar className={classes.appBar}>
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" onClick={onCancel} aria-label="close">
-                        <CloseIcon/>
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Differ Viewer
-                    </Typography>
-                </Toolbar>
-            </AppBar>
             <MonacoDiffEditor
                 height="100%"
                 language="yaml"
